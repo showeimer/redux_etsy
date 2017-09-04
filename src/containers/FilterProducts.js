459845c60604b3271e73db95ab9b12140a41cb3b
showeimer <showeimer@gmail.com>
2017-09-03 22:56:11 -0400

    set up conditional for reducer, mapped Product component to generate products from data, wrote filtering code for prices

--- src/containers/FilterProducts.js
+++ src/containers/FilterProducts.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {filterProducts} from "../actions";
+import {filterProducts} from "../actions/actions";
 
 class FilterProducts extends Component {
     createFilterRadio(value, text) {
--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -6,19 +6,26 @@ import Product from "../components/Product";
 class ProductList extends Component {
 
     render() {
+        // this products property is created by the reducer
         const {products} = this.props;
+
+        const productList = products.map((product) => {
+          return (
+            <Product product={product} key={product.listing_id} />
+          )
+        })
         // Create a dynamically populated list of `<Product />` components
         // Each `<Product />` component should have a single object from the `products` state property (array)
         // applied to the component as a `product` property
         return (
             <ul className="ProductList">
-
+              {productList}
             </ul>
         );
     }
 }
 
-// Using the `mapStateToProps` function, filter the array stored in the 
+// Using the `mapStateToProps` function, filter the array stored in the
 // state `products` property based on 3 criterea:
 // - `underTwenty`
 // - `overTwenty`
@@ -26,12 +33,12 @@ class ProductList extends Component {
 const mapStateToProps = function(state) {
     let products;
     // complete the `if else` statement including conditions and `products` value
-    if () {
-
-    } else if () {
-
+    if (state.filter === 'underTwenty') {
+      products = state.products.filter(product => parseInt(product.price) < 20)
+    } else if (state.filter === 'overTwenty') {
+      products = state.products.filter(product => parseInt(product.price) > 20)
     } else {
-
+      products = state.products;
     }
 
     return {products: products}
